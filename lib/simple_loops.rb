b9b7725b69199debb2daabc7db1ba939d930a62d
Chad Palmer <chad.palmer@yale.edu>
2020-05-14 22:03:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,8 +4,20 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_five_times(message)
-  5.times do
+def loop_message_n_times(message, n)
+  n.times do
     puts message
   end
 end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end
+end
+
+def return_string_array(array)
+  count = 0 
+  
